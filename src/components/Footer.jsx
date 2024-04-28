10b4eb0689a24e61ac0f767147d214d66dc478f7
cherrqq1903 <87109468+cherrqq1903@users.noreply.github.com>
2024-04-28 08:07:53 -0400

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+const Footer = () => (
+    <footer style={{
+        backgroundColor: 'black', 
+        position: 'relative', 
+        width: '100%', 
+        bottom: '0', 
+        color: 'white', 
+        fontSize: '20px', 
+        textAlign: 'center',
+        marginTop: '0',
+        marginBottom: '0',
+        paddingBottom: '0'
+    }}>
+    </footer>
+);
+
+export default Footer
