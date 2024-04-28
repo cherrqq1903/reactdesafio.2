e245aa79c05f2b55f8641593b2c45d67273ef80e
cherrqq1903 <87109468+cherrqq1903@users.noreply.github.com>
2024-04-28 08:09:18 -0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <>
       <div className="App" style={{backgroundImage:"url(/pizza-banner.jpg)", backgroundRepeat:'no-repeat', WebkitBackgroundSize:'cover'}}>
         <h1 className="text-center text-light" >¡Pizzería Mamma Mia!</h1>
-        <h3 className="text-center text-light">¡Tenemos las mejores pizzas que podrás encontrar!</h3>
+        <h3 className="text-center text-light">¡Tenemos las mejores pizzas!</h3>
         <hr/>
       </div>
       <Catalog/>
